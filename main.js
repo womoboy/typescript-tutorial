082bed38a1dc4995aa323eee57d2313259b2d877
reza2boyce <reza2boyce@gmail.com>
2022-01-08 13:21:52 +0330

    connect to git

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var array1;
+array1 = [4, 8, 10, 11, 20, 70, 55];
+console.log(array1.length);
+console.log(array1[2]);
---
+++ main.ts
@@ -0,0 +1,7 @@
+export {}
+
+let array1: number[];
+array1 = [4,8,10,11,20,70,55];
+
+console.log(array1.length);
+console.log(array1[2]);
